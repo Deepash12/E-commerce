0f7998cbf1a941b09043b5965ea3e16cd9243364
ixDeepeshRathi <deepesh.rathi@innovatechs.com>
2026-01-05 18:22:02 +0530

    implement the Register User Functionality

---
+++ src/main/java/com/example/E/commerce/E_commerce/DTO/RegisterRequestDTO.java
@@ -0,0 +1,30 @@
+package com.example.E.commerce.E_commerce.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.math.BigInteger;
+
+@Setter
+@Getter
+public class RegisterRequestDTO
+{
+    @NotBlank(message = "Username is Required")
+    private String username;
+    @NotBlank(message = "Password is Required")
+    private String password;
+    private String roles;
+    @Pattern(
+            regexp = "^[6-9]\\d{9}$",
+            message = "Invalid phone number"
+    )
+    private BigInteger phoneNumber;
+    @Email(message = "Invalid Email is Written")
+    @NotBlank(message = "Email is Required")
+    private String email;
+
+}
--- src/main/java/com/example/E/commerce/E_commerce/Service/AuthService.java
+++ src/main/java/com/example/E/commerce/E_commerce/Service/AuthService.java
@@ -1,29 +1,53 @@
 package com.example.E.commerce.E_commerce.Service;
 
-import com.example.E.commerce.E_commerce.Controller.Auth;
-import com.example.E.commerce.E_commerce.DTO.AuthDTO;
+import com.example.E.commerce.E_commerce.DTO.RegisterRequestDTO;
+import com.example.E.commerce.E_commerce.Model.User;
+import com.example.E.commerce.E_commerce.Repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService
 {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    public String registerUser(RegisterRequestDTO registerRequestDTO)
+    {
+        if(userRepository.existsByusername(registerRequestDTO.getUsername()))
+        {
+            throw new RuntimeException("Username Already existed!!!");
+        }
+        if(userRepository.existsByEmail(registerRequestDTO.getEmail()))
+        {
+            throw new RuntimeException("Email Already Existed!!!");
+        }
+
+        User user =  new User();
+        user.setUsername(registerRequestDTO.getUsername());
+        user.setPassword(passwordEncoder.encode(registerRequestDTO.getPassword()));
+        user.setEmail(registerRequestDTO.getEmail());
+        user.setRole(registerRequestDTO.getRoles());
+        user.setPhoneNumber(registerRequestDTO.getPhoneNumber());
+        userRepository.save(user);
+        return "Registered Successfully , Please Login";
+    }
+}
+
 //    private Auth auth;
 //
 //    public AuthService(Auth auth) {
 //        this.auth = auth;
 //    }
-
-    public String userLogin(AuthDTO authDTO)
-    {
-
-        return "";
-    }
-
-    
-
-    public String registerUser()
-    {
-
-        return "";
-    }
-}
+//
+//    public String userLogin(AuthDTO authDTO)
+//    {
+//
+//        return "";
+//    }
