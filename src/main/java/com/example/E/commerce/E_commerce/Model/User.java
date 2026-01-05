4c534abc3e6bca9422a231f3d6ced8c28b2837af
ixDeepeshRathi <deepesh.rathi@innovatechs.com>
2026-01-05 12:22:31 +0530

    Bean issue and circular dependency issue is resolved and db connection issue also resolved

--- src/main/java/com/example/E/commerce/E_commerce/Model/User.java
+++ src/main/java/com/example/E/commerce/E_commerce/Model/User.java
@@ -1,5 +1,7 @@
 package com.example.E.commerce.E_commerce.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import lombok.Data;
 
@@ -7,8 +9,9 @@ import java.math.BigInteger;
 import java.util.Date;
 
 @Data
+@Entity
 public class User {
-
+    @Id
     private Long id;
 
     private String username;
@@ -21,6 +24,62 @@ public class User {
     private BigInteger phoneNumber;
 
     private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public BigInteger getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(BigInteger phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getLastLoggedIn() {
+        return lastLoggedIn;
+    }
+
+    public void setLastLoggedIn(Date lastLoggedIn) {
+        this.lastLoggedIn = lastLoggedIn;
+    }
 
     private Date lastLoggedIn;
 }
