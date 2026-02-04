1d0180e28f7b9cd18e8c9e4cc92bc9eb12f29346
ixDeepeshRathi <deepesh.rathi@innovatechs.com>
2026-02-04 12:30:58 +0530

    Implemebnted the Log-out Functionality

---
+++ src/main/java/com/example/E/commerce/E_commerce/Service/tokenBlackListService.java
@@ -0,0 +1,18 @@
+package com.example.E.commerce.E_commerce.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+@Service
+public class tokenBlackListService {
+    private final Set<String> blacklistedToken = ConcurrentHashMap.newKeySet();
+    public void blacklist(String token)
+    {
+        blacklistedToken.add(token);
+    }
+    public boolean isBlacklisted(String token)
+    {
+        return blacklistedToken.contains(token);
+    }
+}
