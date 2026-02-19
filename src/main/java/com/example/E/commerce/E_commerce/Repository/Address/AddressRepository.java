e20eba2f6e1f59711bbcdb25eebb4e085f0cc570
ixDeepeshRathi <deepesh.rathi@innovatechs.com>
2026-02-19 12:36:31 +0530

    Implement the Select Address and Update Address For User

--- src/main/java/com/example/E/commerce/E_commerce/Repository/Address/AddressRepository.java
+++ src/main/java/com/example/E/commerce/E_commerce/Repository/Address/AddressRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface AddressRepository extends JpaRepository<UserAddresses,Long>
 {
@@ -17,4 +18,6 @@ public interface AddressRepository extends JpaRepository<UserAddresses,Long>
     void cleanDefaultForUser(Long userId);
 
     Page<UserAddresses> findByUser(User user, Pageable pageable);
+
+    Optional<UserAddresses> findByIdAndUser(Long id, User user);
 }
