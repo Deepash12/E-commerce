5bd15fe0f24f0ad8ba60dc395ad9c81eda2c69c3
ixDeepeshRathi <deepesh.rathi@innovatechs.com>
2026-05-13 16:18:09 +0530

    fix the global file upload issue

--- src/main/java/com/example/E/commerce/E_commerce/Service/File/FileService.java
+++ src/main/java/com/example/E/commerce/E_commerce/Service/File/FileService.java
@@ -1,6 +1,10 @@
 package com.example.E.commerce.E_commerce.Service.File;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.example.E.commerce.E_commerce.Exception.BadRequestException;
+import com.example.E.commerce.E_commerce.Security.CloudinaryConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,52 +13,82 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 @Service
 public class FileService
 {
-    private final String uploadDir = "uploads/";
 
-    public String uploadFile(MultipartFile file) throws IOException
-    {
-        // Check if file is empty
-        if (file.isEmpty())
-        {
-            throw new BadRequestException("File cannot be empty");
-        }
 
-        // Allowed image types
-        String contentType = file.getContentType();
 
-        if (contentType == null ||
-                !(contentType.equals("image/jpeg") ||
-                        contentType.equals("image/png")  ||
-                        contentType.equals("image/jpg")  ||
-                        contentType.equals("image/webp")))
-        {
-            throw new BadRequestException(
-                    "Only image files are allowed (jpg, jpeg, png, webp)"
-            );
-        }
 
-        // Create uploads folder if not exists
-        File folder = new File(uploadDir);
+    @Autowired
+    private Cloudinary cloudinary;
 
-        if (!folder.exists())
-        {
-            folder.mkdir();
-        }
+    public String uploadFile(MultipartFile file) throws IOException {
 
-        // Generate unique file name
-        String fileName = System.currentTimeMillis()
-                + "_"
-                + file.getOriginalFilename();
+        Map uploadResult = cloudinary.uploader().upload(
+                file.getBytes(),
+                ObjectUtils.emptyMap()
+        );
 
-        Path filePath = Paths.get(uploadDir, fileName);
-
-        // Copy file
-        Files.copy(file.getInputStream(), filePath);
-
-        return "/uploads/" + fileName;
+        return uploadResult.get("secure_url").toString();
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    private final String uploadDir = "uploads/";
+//
+//    public String uploadFile(MultipartFile file) throws IOException
+//    {
+//        // Check if file is empty
+//        if (file.isEmpty())
+//        {
+//            throw new BadRequestException("File cannot be empty");
+//        }
+//
+//        // Allowed image types
+//        String contentType = file.getContentType();
+//
+//        if (contentType == null ||
+//                !(contentType.equals("image/jpeg") ||
+//                        contentType.equals("image/png")  ||
+//                        contentType.equals("image/jpg")  ||
+//                        contentType.equals("image/webp")))
+//        {
+//            throw new BadRequestException(
+//                    "Only image files are allowed (jpg, jpeg, png, webp)"
+//            );
+//        }
+//
+//        // Create uploads folder if not exists
+//        File folder = new File(uploadDir);
+//
+//        if (!folder.exists())
+//        {
+//            folder.mkdir();
+//        }
+//
+//        // Generate unique file name
+//        String fileName = System.currentTimeMillis()
+//                + "_"
+//                + file.getOriginalFilename();
+//
+//        Path filePath = Paths.get(uploadDir, fileName);
+//
+//        // Copy file
+//        Files.copy(file.getInputStream(), filePath);
+//
+//        return "/uploads/" + fileName;
+//    }
 }
